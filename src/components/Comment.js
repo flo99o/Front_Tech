cb843fc38aa1ae1107d618c6d6136b996366d89d
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-09-30 11:06:47 +0200

    did comment section layout

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,8 +2,13 @@ import React from 'react'
 
  const Comment = () => {
     return (
-        <section className="comments">
-            
+        <section className="comment">
+            <div className="comment__content">
+                <p className="comment__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor soluta, eaque doloremque sunt similique sit totam aliquam? Placeat!</p>
+                <img className="comment__image" src="" alt=""/>
+                <h6 className="comment__username">Paul Lagay</h6>
+                <span className="comment__userjob">CEO de BNP</span>
+            </div>
         </section>
     )
 }
