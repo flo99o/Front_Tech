816f6659d81c9a5d687f191860af4adaa627820d
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-10-01 15:17:44 +0200

    finish to build all homepage layout expect navbar

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import userPhoto from '../assets/user_profile/man.jpg'
 
  const Comment = () => {
     return (
         <section className="comment">
             <div className="comment__content">
                 <p className="comment__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor soluta, eaque doloremque sunt similique sit totam aliquam? Placeat!</p>
-                <img className="comment__image" src="" alt=""/>
+                <img className="comment__image" src={userPhoto} alt=""/>
                 <h6 className="comment__username">Paul Lagay</h6>
                 <span className="comment__userjob">CEO de BNP</span>
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,51 @@
-import React from 'react'
+import React from "react";
 
- const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="container">
-                <div className="about">
-                    <img classname="about__image" src="" alt="logo de l'entreprise"/>
-                    <p className="about__presentation">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, facere sint. Sunt, quos? Repellat explicabo.</p>
-                    <ul className="media-list">
-                        <li className="fb"></li>
-                        <li className="twitter"></li>
-                        <li className="linkedin"></li>
-                        <li className="pinterest"></li>
-                    </ul>
-                </div>
-                <div className="quick-links">
-                <h6 className="title">Quick links</h6>
-                    <a href="">Post New job</a>
-                    <a href="">jobs List</a>
-                    <a hretf="">Employer List</a>
-                    <a href=""></a>
-                </div>
-                <div className="tranding-jobs">
-                <h6 className="title">Tranding Jobs</h6>
-                    <a href="">Designer</a>
-                    <a href="">UI & UX Expert</a>
-                    <a href="">Developer</a>
-                    <a href="">IOS developer</a>
-                    <a href="">Business Developer</a>
-                </div>
-                <div className="newsletter">
-                    <h6 className="title">Newsletter</h6>
-                    <p>Subscribe to Lawson to get all latest Job, Resume, Company Listing & Blog post to stay update.</p>
-                    <form action="" className="newsletter__form">
-                        <input type="email" placeholder="Entrer votre adresse mail"/>
-                        <button className="submit">
-                            <img src="" alt="logo d'une enveloppe"/>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="about">
+          <div className="logo logo--footer">TechWork</div>
+          <p className="about__presentation">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam,
+            facere sint. Sunt, quos? Repellat explicabo.
+          </p>
+          <ul className="media-list">
+            <li className="fb"></li>
+            <li className="twitter"></li>
+            <li className="linkedin"></li>
+            <li className="pinterest"></li>
+          </ul>
+        </div>
+        <div className="quick-links">
+          <h6 className="title">Quick links</h6>
+          <ul>
+            <li href="">Post New job</li>
+            <li href="">jobs List</li>
+            <li hretf="">Employer List</li>
+            <li href=""></li>
+          </ul>
+        </div>
+        <div className="tranding-jobs">
+          <h6 className="title">Tranding Jobs</h6>
+          <ul>
+            <li href="">Designer</li>
+            <li href="">UI & UX Expert</li>
+            <li href="">Developer</li>
+            <li href="">IOS developer</li>
+            <li href="">Business Developer</li>
+          </ul>
+        </div>
+        <div className="newsletter">
+          <h6 className="title">Newsletter</h6>
+          <p>
+            Subscribe to Lawson to get all latest Job, Resume, Company Listing &
+            Blog post to stay update.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
