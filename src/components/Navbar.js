f5d326b48f5a11f8d499a5410688dc5e70338b69
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-10-15 12:15:58 +0200

    navbar is functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import $ from "jquery";
 import iconSet from "../assets/selection.json";
 import IcomoonReact, { iconList } from "icomoon-react";
@@ -7,11 +7,9 @@ import IcomoonReact, { iconList } from "icomoon-react";
 import briefcase from "../assets/logo_nav/mallette.svg";
 import registration from "../assets/logo_nav/registration.svg";
 
-const getLogout = () => {
-  localStorage.clear();
-};
 
 const NavBar = () => {
+  let history = useHistory()
   const [isLogged, setIsLogged] = useState("");
   const [userID, setUserID] = useState("");
 
@@ -23,11 +21,18 @@ const NavBar = () => {
       }
     }
 
-    $(".cross-closed-menu").on("click", () => {
-      console.log("hello");
+    $( ".cross-closed-menu, li.navigation__items" ).on("click", () => {
+      console.log('hello');
       $(".navigation").hide();
     });
   }, [isLogged, userID]);
+
+  const getLogout = () => {
+    console.log("logout");
+    localStorage.clear();
+    history.push("/")
+  };
+  
 
   return (
     <nav className="navigation">
@@ -58,14 +63,13 @@ const NavBar = () => {
             </Link>
           </li>
         ) : (
-          <li className="navigation__items">
+          <li onClick={() => getLogout()} className="navigation__items">
             <Link to="/">
               <IcomoonReact
                 iconSet={iconSet}
                 color="#444"
                 size={15}
                 icon="log-out"
-                onClick={getLogout()}
               />
               <p>Se déconnecter </p>
             </Link>
@@ -83,10 +87,6 @@ const NavBar = () => {
             <img src={briefcase} alt="malette" />
             <p>Les offres d'emplois </p>
           </Link>
-        </li>
-        <li className="navigation__items">
-          <IcomoonReact iconSet={iconSet} color="#444" size={15} icon="help" />
-          <p>FAQ </p>
         </li>
       </ul>
       <span className="cross-closed-menu">&#10006;</span>
