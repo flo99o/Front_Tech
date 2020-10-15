8af8b53422d4a4352e20383148c51369a0bfcbc8
Florence Alonzeau <florencealonzeau18@gmail.com>
2020-10-15 11:06:10 +0200

    routes front

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import {Route} from 'react-router-dom'
 
 export const ProtectedRoutes = ({component: Component, ...rest}) => {
+      
+
     return (
         <div>
             <Route {...rest} render={
--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
 export const Unauthorized = (userType) => {
+
 
     return (
         <div>
